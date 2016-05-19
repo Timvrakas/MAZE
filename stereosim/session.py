e955ee068a3af672083ee22970f1768d2d96874b
nikhilkalige <nikhilkalige@gmail.com>
2016-05-19 14:17:55 -0700

    Create a class for session

---
+++ stereosim/session.py
@@ -0,0 +1,36 @@
+from sys import path
+import configparser
+
+
+class Session:
+    CONFIG_FILE = '~/config/stereosim.ini'
+
+    def __init__(self):
+        pass
+
+    def setup(self):
+        self.config = configparser.ConfigParser()
+        if not path.isfile(self.CONFIG_FILE):
+            self.config['DEFAULT'] = {
+                'SessionNumber': 1,
+                'ImageCount': 1
+            }
+            with open(self.CONFIG_FILE, 'w') as f:
+                self.config.write(f)
+
+        else:
+            self.config.read(self.CONFIG_FILE)
+
+    def session_number(self, new=False):
+        session_number = self.config['DEFAULT']['SessionNumber']
+        if new:
+            session_number += 1
+
+        return session_number
+
+    def image_count(self, inc=False):
+        count = self.config['DEFAULT']['ImageCount']
+        if inc:
+            count += 1
+        else:
+            return count
