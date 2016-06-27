671d0d4187a3668111e65e9852c3eb1d2952fe63
Andy Winhold <andrew.winhold@asu.edu>
2016-06-27 14:06:05 -0700

    Adding mosaic_session to capture.py

--- stereosim/capture.py
+++ stereosim/capture.py
@@ -14,21 +14,22 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-def point(cam, ptu, session, az=False, el=False):
-    if az=False or el=False:
+def point(cam, ptu, session, az=None, el=None):
+    if az==None and el==None:
         az = int(input('Enter Azimuth: '))
         el = int(input('Enter Elevation: '))
-        ptu.pan_angle(az)
-        ptu.tilt_angle(el)
-        pp = ptu.pan()
-        tp = ptu.tilt()
-        print('PP: ', pp, '\n', 'TP: ', tp)
-    else:
-        ptu.pan_angle(az)
-        ptu.tilt_angle(el)
-        pp = ptu.pan()
-        tp = ptu.tilt()
-        print('PP: ', pp, '\n', 'TP: ', tp)
+    elif az==None and el!=None:
+        el = el
+        az = int(input('Elevation : {} \n Enter Azimuth: '.format(el)))
+    elif az!=None and el==None:
+        az = az
+        el = int(input('Azimuth : {} \n Enter Elevation: '.format(az)))
+
+    ptu.pan_angle(az)
+    ptu.tilt_angle(el)
+    pp = ptu.pan()
+    tp = ptu.tilt()
+    print('PP: ', pp, '\n', 'TP: ', tp)
 
 
 def settings(cam, ptu, session):
@@ -37,22 +38,47 @@ def settings(cam, ptu, session):
     print("Press `s` key if you want to get the camera parameters")
 
 
-def capture(cam, ptu, session, filepath=False, filename=False):
-    if filepath=False or filename=False:
-        print('Capturing an image...')
-        file_path = session.get_folder_path()
-        file_name = session.get_file_name()
-        cam.capture_image(file_path, file_name)
-        print('Image Captured.')
-        session.image_count(inc=True)
-    else: 
-        print('Capturing an image...')
-        cam.capture_image(filepath, filename)
-        print('Image Captured.')
-        session.image_count(inc=True)
+def capture(cam, ptu, session):
+    print('Capturing an image...')
+    file_path = session.get_folder_path()
+    file_name = session.get_file_name()
+    cam.capture_image(file_path, file_name)
+    print('Image Captured.')
+    session.image_count(inc=True)
 
-def mosaic(cam, ptu, filepath, positions):
-    """ Capture a mosiac around the position defined by positions
+def pos_arr(pos):
+    '''Make an (x,2) position array from a comma seperated list 
+       where each row provides az and el data. There must be an even number of values.
+       Used when mosaic_session is chosen.
+       
+    Parameters
+    ----------
+    
+    pos : input string
+        comma seperated list of az and el.
+        ex: az,el,az,el,az,el.
+        
+    Returns
+    -------
+    
+    pos_arr : array
+        An (x,2) array where column 0 is az and column 1 is el.
+        ex: [(az, el),
+             (az, el),
+             (az, el)]
+    '''
+    lst = list(map(int, pos.split(',')))
+    length = len(lst)
+    while (num % 2) != 0:
+        print('{} data points provided, an even number of az and el data points is required please try again.'.format(length))
+    arr = np.asarray(lst)
+    arr = arr.reshape((len(lst)/2, 2))
+
+    return arr
+
+
+def mosaic(cam, ptu, session, positions):
+    """ Capture a mosiac based on the positions provided
 
     Parameters
     ----------
@@ -61,11 +87,10 @@ def mosaic(cam, ptu, filepath, positions):
         Instance of 'StereoCamera'
     ptu : PTU
         Instance of 'PTU'
-    filepath : filepath
-        path to where the image(s) will be stored
     positions : array
-        azimuth/elevation locations to image
-        ex: positions = [(az, el), (az, el), (az, el)]
+        Azimuth/elevation locations to image.
+        **NOTE: Each consecutive az or el value is added to the previous.
+        Ex: if 1st az is 10, and 2nd az is 12, after reading in 2nd az the position will be 22.
 
     Returns
     -------
@@ -73,9 +98,6 @@ def mosaic(cam, ptu, filepath, positions):
     TBD :
     """ 
     file_name_count = 1
-
-    if not os.path.isdir(filepath):
-        raise Exception("Invalid path: {}".format(filepath))
 
     # Move to the initial position
     ptu.pan_angle(positions[0][0])
@@ -91,7 +113,7 @@ def mosaic(cam, ptu, filepath, positions):
         filename = str(file_name_count).zfill(5)
         logger.info('Current Position:- Az: {}, El: {}, Current File:- {}'.format(curr_az, curr_el, filename))
         
-        camera_files = capture(cam, ptu, session, filepath=filepath, filename=filename)
+        camera_files = capture(cam, ptu, session)
         logger.info(camera_files)
 
         # removed because they are in point()
@@ -163,11 +185,26 @@ def main():
     s.detect_cameras()
     ptu.connect()
 
-    with start_session() as session:
-        while True:
-            command_input = input('> ')
-            test_case(command_input, s, ptu, session)
+    session_type = input('Begin regular_session or mosaic_session? ')
+    while session_type not in ['regular_session', 'mosaic_session']:
+        print('Input not applicable, please choose either "regular_session" or "mosaic_session"')
+        session_type = input('regular_session or mosaic_session? ')
 
+    with start_session() as session:
+        if session_type==regular_session:
+                while True:
+                    command_input = input('> ')
+                    test_case(command_input, s, ptu, session)
+        elif session_type==mosaic_session:
+            while True:
+                    positions = input('Input comma separated list of az/el positions for mosaic, enter "feeling lucky" for default positions: ')
+                    # Add a default option that uses predetermined steps.
+                    if positions == 'default':
+                        positions = 0,0,15,0,15,0,0,-15,-15,0,-15,0
+                        print('positions : (0, 0), (15, 0), (15, 0), (0, -15), (-15, 0), (-15, 0)')
+                    positions = pos_arr(positions)
+                    mosaic(cam, ptu, session, positions)
+        
 
 if __name__ == "__main__":
     logger = logging.getLogger()
