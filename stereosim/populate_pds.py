972b267a1f478273ba7444a3ae35ff422a654d68
--global <andrew.winhold@asu.edu>
2017-05-17 23:46:25 +0000

    work on PDS label, now adds comment lines refs#38

--- stereosim/populate_pds.py
+++ stereosim/populate_pds.py
@@ -7,6 +7,7 @@ from PIL import Image
 import exifread
 import argparse
 import os.path
+import six
 import yaml
 import pvl
 import io
@@ -24,9 +25,8 @@ class PDSGenerator(PDS3Image):
         new_ar[1, :, :] = np_ar[:, :, 1]
         new_ar[2, :, :] = np_ar[:, :, 2]
         new_ar = new_ar.astype('>i2')
+        # call __init__ of planetaryimage's class
         super().__init__(new_ar)
-        #self._create_label(new_ar)
-        #self = PDS3Image(new_ar)
         self.filename = os.path.splitext(filepath)[0]
         if self._label_file_exists():
             self.label = self._update_labelPDSG(pds_date)
@@ -37,20 +37,21 @@ class PDSGenerator(PDS3Image):
         Checks whether label file for given JPG Images exists or not.
 
         Returns:
-        1: if label file is present in the same path
-        0: if label file does not exist in the same path
+        True: if label file is present in the same path
+        False: if label file does not exist in the same path
         """
         self.label_file = self.filename + '.lbl'
         if os.path.isfile(self.label_file):
-            return 1
+            return True
         else:
-            return 0
+            return False
 
     def _update_labelPDSG(self, pds_date):
         """
-        Updates label by adding groups into it.
+        Updates label by adding groups and comments into it.
         1. PTU_ARTICULATION_STATE
-        2. CAHVOR_CAMERA_MODEL
+        2. CAHVOR_CAMERA_MODEL_LEFT
+        3. And so on
 
         Returns:
         label: PVLModule
@@ -61,12 +62,11 @@ class PDSGenerator(PDS3Image):
                 self.yaml_data = yaml.load(fp)
             except yaml.YAMLError as exc:
                 print(exc)
-        self.label = self._add_group('IDENTIFICATION_DATA_ELEMENTS')
         self.label = self._add_group('PTU_ARTICULATION_STATE')
         self.label = self._add_group('CAHVOR_CAMERA_MODEL_LEFT')
         self.label = self._add_group('CAHVOR_CAMERA_MODEL_RIGHT')
-        self.label = self._add_group('PHOTOGRAMMETRY_CAMERA_MODEL_LEFT')
-        self.label = self._add_group('PHOTOGRAMMETRY_CAMERA_MODEL_RIGHT')
+        self.label = self._add_group('COLLINEAR_CAMERA_MODEL_LEFT')
+        self.label = self._add_group('COLLINEAR_CAMERA_MODEL_RIGHT')
         self.label['IMAGE']['IMAGE_CREATION_TIME'] =  pds_date
         return self.label
 
@@ -80,24 +80,11 @@ class PDSGenerator(PDS3Image):
             Name of the group to be added.
 
         Returns:
+        --------
         label: PVLModule
             Returns label after adding PVLGroup in it.
-        """
-        if group_name == 'IDENTIFICATION_DATA_ELEMENTS':
-            self.label['BEGIN_GROUP'] = 'IDENTIFICATION_DATA_ELEMENTS'
-            self.label['DATA_SET_ID'] = 'UNK'
-            self.label['PRODUCT_ID'] = 'UNK'
-            self.label['INSTRUMENT_HOST_NAME'] = 'MARS 2020'
-            self.label['INSTRUMENT_NAME'] = 'STEREOSIM'
-            self.label['TARGET_NAME'] = 'MARS'
-            self.label['START_TIME'] = 'UNK'
-            self.label['STOP_TIME'] = 'UNK'
-            self.label['SPACECRAFT_CLOCK_START_COUNT'] = 'UNK'
-            self.label['SPACECRAFT_CLOCK_STOP_COUNT'] = 'UNK'
-            self.label['PRODUCT_CREATION_TIME'] = 'UNK'
-            self.label['END_GROUP'] = 'IDENTIFICATION_DATA_ELEMENTS'
-
-        elif group_name == 'PTU_ARTICULATION_STATE':
+        """  
+        if group_name == 'PTU_ARTICULATION_STATE':
             self.label['BEGIN_GROUP'] = 'PTU_ARTICULATION_STATE'
             self.label['ARTICULATION_DEVICE_ID'] = "PTU"
             self.label['ARTICULATION_DEVICE_NAME'] = "FLIR Pan-Tilt Unit"
@@ -108,7 +95,7 @@ class PDSGenerator(PDS3Image):
             self.label['END_GROUP'] = 'PTU_ARTICULATION_STATE'
 
         elif group_name == 'CAHVOR_CAMERA_MODEL_LEFT' and self.yaml_data['Camera'] == 'LEFT':
-            self.label['BEGIN_GROUP'] = 'CAHVOR_CAMERA_MODEL_LEFT'
+            self.label['BEGIN_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
             self.label['MODEL_TYPE'] = 'CAHVOR'
             self.label['MODEL_COMPONENT_ID'] = ["C", "A", "H", "V", "O", "R", "Hc", "Vc", "Vs", "V1"]
             self.label['MODEL_COMPONENT_NAME'] = ["CENTER", "AXIS",
@@ -137,11 +124,12 @@ class PDSGenerator(PDS3Image):
             self.label['MODEL_COMPONENT_6'] = R
             self.label['MODEL_COMPONENT_7'] = Hc
             self.label['MODEL_COMPONENT_8'] = Vc
-            self.label['MODEL_COMPONENT_9'] = V1
-            self.label['END_GROUP'] = 'CAHVOR_CAMERA_MODEL_LEFT'
+            self.label['MODEL_COMPONENT_9'] = Vs
+            self.label['MODEL_COMPONENT_10'] = V1
+            self.label['END_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
 
         elif group_name == 'CAHVOR_CAMERA_MODEL_RIGHT' and self.yaml_data['Camera'] == 'RIGHT':
-            self.label['BEGIN_GROUP'] = 'CAHVOR_CAMERA_MODEL_RIGHT'
+            self.label['BEGIN_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
             self.label['MODEL_TYPE'] = 'CAHVOR'
             self.label['MODEL_COMPONENT_ID'] = ["C", "A", "H", "V", "O", "R", "Hc", "Vc", "Vs", "V1"]
             self.label['MODEL_COMPONENT_NAME'] = ["CENTER", "AXIS",
@@ -170,59 +158,62 @@ class PDSGenerator(PDS3Image):
             self.label['MODEL_COMPONENT_6'] = R
             self.label['MODEL_COMPONENT_7'] = Hc
             self.label['MODEL_COMPONENT_8'] = Vc
-            self.label['MODEL_COMPONENT_9'] = V1
-            self.label['END_GROUP'] = 'CAHVOR_CAMERA_MODEL_RIGHT'
+            self.label['MODEL_COMPONENT_9'] = Vs
+            self.label['MODEL_COMPONENT_10'] = V1
+            self.label['END_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
 
-        elif group_name == 'PHOTOGRAMMETRY_CAMERA_MODEL_LEFT' and self.yaml_data['Camera'] == 'LEFT':
-            self.label['BEGIN_GROUP'] = 'PHOTOGRAMMETRY_CAMERA_MODEL_LEFT'
-            self.label['MODEL_TYPE'] = 'PHOTOGRAMMETRY'
-            self.label['MODEL_COMPONENT_ID'] = ["C", "F", "PxS", "ImS", "P", "ANG"]
+        elif group_name == 'COLLINEAR_CAMERA_MODEL_LEFT' and self.yaml_data['Camera'] == 'LEFT':
+            self.label['BEGIN_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
+            self.label['MODEL_TYPE'] = 'COLLINEAR'
+            self.label['MODEL_COMPONENT_ID'] = ["C", "F", "PxS", "P", "ANG", "k"]
             self.label['MODEL_COMPONENT_NAME'] = ["CENTER", "FOCAL_LENGTH", "PIXEL_SIZE",
-                                                      "IMAGE_SIZE", "PRINICIPAL", "ROTATION_ANGLES"]
+                                                  "PRINICIPAL", "ROT_ANG[OMEGA,PHI,KAPPA]", "DISTORTION_COEFFICIENTS"]
 
             C = [3.451904, 3.258335, 1.254338]
             F = 28.76033845
-            ImS = [3072, 2048]
             PxS = [0.0074, 0.0074]
             P = [-0.098722821, -0.125838347]
-            ANG = [-72.29916094, 44.2841281, 166.5327547]
+            ANG = [-72.2993175, 44.2841281, 166.5327547]
+            k = [0, 0.000200218923, -0.000000130736213]
 
 
             self.label['MODEL_COMPONENT_1'] = C
             self.label['MODEL_COMPONENT_2'] = F
             self.label['MODEL_COMPONENT_3'] = PxS
-            self.label['MODEL_COMPONENT_4'] = ImS
-            self.label['MODEL_COMPONENT_5'] = P
-            self.label['MODEL_COMPONENT_6'] = ANG
-            self.label['END_GROUP'] = 'PHOTOGRAMMETRY_CAMERA_MODEL_LEFT'  
+            self.label['MODEL_COMPONENT_4'] = P
+            self.label['MODEL_COMPONENT_5'] = ANG
+            self.label['MODEL_COMPONENT_6'] = k
+            self.label['END_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'  
 
-        elif group_name == 'PHOTOGRAMMETRY_CAMERA_MODEL_RIGHT' and self.yaml_data['Camera'] == 'RIGHT':
-            self.label['BEGIN_GROUP'] = 'PHOTOGRAMMETRY_CAMERA_MODEL_RIGHT'
-            self.label['MODEL_TYPE'] = 'PHOTOGRAMMETRY'
-            self.label['MODEL_COMPONENT_ID'] = ["C", "F", "PxS", "ImS", "P", "ANG"]
+        elif group_name == 'COLLINEAR_CAMERA_MODEL_RIGHT' and self.yaml_data['Camera'] == 'RIGHT':
+            self.label['BEGIN_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'
+            self.label['MODEL_TYPE'] = 'COLLINEAR'
+            self.label['MODEL_COMPONENT_ID'] = ["C", "F", "PxS", "P", "ANG", "k"]
             self.label['MODEL_COMPONENT_NAME'] = ["CENTER", "FOCAL_LENGTH", "PIXEL_SIZE",
-                                                      "IMAGE_SIZE", "PRINICIPAL", "ROTATION_ANGLES"]
+                                                  "PRINICIPAL", "ROT_ANG[OMEGA,PHI,KAPPA]", "DISTORTION_COEFFICIENTS"]
 
             C = [3.451904, 3.258335, 1.254338]
             F = 28.9499023
-            ImS = [3072, 2048]
             PxS = [0.0074, 0.0074]
             P = [-0.14548874, -0.1097175]
-            ANG = [-72.2993175, 44.2841281, 166.5327547]
+            ANG = [-72.29916094, 44.28412813, 166.5327617]
+            k = [0, 0.000204968252, -0.000000133968289]
 
             self.label['MODEL_COMPONENT_1'] = C
             self.label['MODEL_COMPONENT_2'] = F
             self.label['MODEL_COMPONENT_3'] = PxS
-            self.label['MODEL_COMPONENT_4'] = ImS
-            self.label['MODEL_COMPONENT_5'] = P
-            self.label['MODEL_COMPONENT_6'] = ANG
-            self.label['END_GROUP'] = 'PHOTOGRAMMETRY_CAMERA_MODEL_RIGHT'  
+            self.label['MODEL_COMPONENT_4'] = P
+            self.label['MODEL_COMPONENT_5'] = ANG
+            self.label['MODEL_COMPONENT_6'] = k
+            self.label['END_GROUP'] = 'GEOMETRIC_CAMERA_MODEL'  
 
 
         stream = io.BytesIO()
-        pvl.dump(self.label, stream)
-        stream.seek(0)
-        label_list = list(pvl.load(stream))
+        with open(self.filename + '.IMG', 'w+b') as stream:
+            # use our own PDS3 encoder class so we can add comment lines
+            pvl.dump(self.label, stream, cls=PDS3LabelEncoder)
+            stream.seek(0)
+            label_list = list(pvl.load(stream))
         l = len(label_list)
         label_list[l-2], label_list[l-1] = label_list[l-1], label_list[l-2]
         label = pvl.PVLModule(label_list)
@@ -249,23 +240,33 @@ class PDSGenerator(PDS3Image):
             lines = array.shape[0]
             line_samples = array.shape[1]
         record_bytes = line_samples * array.itemsize
+        print("array.itemsize:", array.itemsize)
         label_module = pvl.PVLModule([
             ('ODL_VERSION_ID', 'ODL3'),
             ('RECORD_TYPE', 'FIXED_LENGTH'),
             ('RECORD_BYTES', record_bytes),
             ('LABEL_RECORDS', 1),
+            ('DATA_SET_ID', 'UNK'),
+            ('PRODUCT_ID', 'UNK'),
+            ('INSTRUMENT_HOST_NAME','MARS 2020'),
+            ('INSTRUMENT_NAME', 'STEREOSIM'),
+            ('TARGET_NAME','MARS'),
+            ('START_TIME', 'UNK'),
+            ('STOP_TIME', 'UNK'),
+            ('SPACECRAFT_CLOCK_START_COUNT', 'UNK'),
+            ('SPACECRAFT_CLOCK_STOP_COUNT', 'UNK'),
+            ('PRODUCT_CREATION_TIME', 'UNK'),
             ('^IMAGE', 1),
             ('IMAGE',
-                {'BANDS': bands,
-                 'MAXIMUM': 'UNK',
-                 'BAND_STORAGE_TYPE': 'BAND_SEQUENTIAL',
-                 'FIRST_LINE': 1,
-                 'FIRST_LINE_SAMPLE': 1,
-                 'LINES': lines,
-                 'LINE_SAMPLES': line_samples,
-                 'SAMPLE_BITS': array.itemsize * 8,
-                 'SAMPLE_TYPE': 'MSB_INTEGER'})
-            ])
+                pvl.PVLModule([('BANDS', bands),
+                 ('BAND_STORAGE_TYPE', 'BAND_SEQUENTIAL'),
+                 ('FIRST_LINE', 1),
+                 ('FIRST_LINE_SAMPLE', 1),
+                 ('LINES', lines),
+                 ('LINE_SAMPLES', line_samples),
+                 ('SAMPLE_BITS', array.itemsize * 8),
+                 ('SAMPLE_TYPE', 'MSB_INTEGER')])
+            )])
         return self._update_label(label_module, array)
 
     def _update_label(self, label, array):
@@ -294,7 +295,6 @@ class PDSGenerator(PDS3Image):
         label_sz = len(serial_label)
         image_pointer = int(label_sz / label['RECORD_BYTES']) + 1
 
-        #label['/* IDENTIFICATION DATe ELEMENTS */'] = 'None'
         label['^IMAGE'] = image_pointer + 1
         label['LABEL_RECORDS'] = image_pointer
         #label['IMAGE']['MEAN'] = mean
@@ -341,6 +341,114 @@ class PDSGenerator(PDS3Image):
         pds_date = yr + '-' + mo + '-' + d + 'T' + hr + ':' + m + ':' + s + '.sssZ'
         print('date pds format:', pds_date)
         return pds_date
+
+    def _save(self, file_to_write, overwrite):
+        """
+        Redefine _save() used in PDS3Image() so it does not overwrite the comment
+        lines we add in with PDS3LabelEncoder()
+        """
+##        if overwrite:
+##            file_to_write = self.filename
+##        elif os.path.isfile(file_to_write):
+##            msg = 'File ' + file_to_write + ' already exists !\n' + \
+##                  'Call save() with "overwrite = True" to overwrite the file.'
+##            raise IOError(msg)
+
+##        with open(file_to_write, 'a+b') as stream:
+        encoder = PDS3LabelEncoder
+        serial_label = pvl.dumps(self.label, cls=encoder)
+        label_sz = len(serial_label)
+        image_pointer = int(label_sz / self.label['RECORD_BYTES']) + 1
+        self.label['^IMAGE'] = image_pointer + 1
+
+        if self._sample_bytes != self.label['IMAGE']['SAMPLE_BITS'] * 8:
+            self.label['IMAGE']['SAMPLE_BITS'] = self.data.itemsize * 8
+
+        sample_type_to_save = self.DTYPES[self._sample_type[0] + self.dtype.kind]
+        self.label['IMAGE']['SAMPLE_TYPE'] = sample_type_to_save
+
+        if len(self.data.shape) == 3:
+            self.label['IMAGE']['BANDS'] = self.data.shape[0]
+            self.label['IMAGE']['LINES'] = self.data.shape[1]
+            self.label['IMAGE']['LINE_SAMPLES'] = self.data.shape[2]
+        else:
+            self.label['IMAGE']['BANDS'] = 1
+            self.label['IMAGE']['LINES'] = self.data.shape[0]
+            self.label['IMAGE']['LINE_SAMPLES'] = self.data.shape[1]
+
+        diff = 0
+        if len(pvl.dumps(self.label, cls=encoder)) != label_sz:
+            diff = abs(label_sz - len(pvl.dumps(self.label, cls=encoder)))
+        pvl.dump(self.label, file_to_write, cls=encoder)
+        offset = image_pointer * self.label['RECORD_BYTES'] - label_sz
+        stream = open(file_to_write, 'a')
+        for i in range(0, offset+diff):
+            stream.write(" ")
+
+        if (self._bands > 1 and self._format != 'BAND_SEQUENTIAL'):
+            raise NotImplementedError
+        else:
+            self.data.tofile(stream, format='%' + self.dtype.kind)
+        stream.close()
+
+class PDS3LabelEncoder(pvl.PVLEncoder):
+    """
+    Redefine some of PVLEncoder's methods as well as PDSLabelEncoder methods so we can add comment lines between sections
+    """
+    begin_comment = b'/* '
+    end_comment = b' */'
+    new_line = b'\r\n'
+    begin_group = b'GROUP'
+    begin_object = b'OBJECT'
+
+    comments = {
+        'DATA_SET_ID': b'IDENTIFICATION DATA ELEMENTS',
+        'FILTER_NAME': b'DESCRIPTIVE DATA ELEMENTS'
+    }
+
+    # Allow comments to be encoded as well
+    def encode_block(self, block, level, stream):
+        for key, value in six.iteritems(block):
+            if key in self.comments:
+                self.encode_comment(key, self.comments[key], level, stream)
+            self.encode_statement(key, value, level, stream)
+
+    def encode_comment(self, key, comment, level, stream):
+        stream.write(self.newline)
+        stream.write(self.begin_comment)
+        stream.write(comment)
+        stream.write(self.end_comment)
+        stream.write(self.newline)
+        stream.write(self.newline)
+
+    # These four methods are brought in from pvl.PDSLabelEncoder to help format the label
+    # in the way PDS/IDL desires.
+    def _detect_assignment_col(self, block, indent=0):
+        if not block:
+            return 0
+        block_items = six.iteritems(block)
+        return max(self._key_length(k, v, indent) for k, v in block_items)
+
+    def _key_length(self, key, value, indent):
+        length = indent + len(key)
+
+        if isinstance(value, dict):
+            indent += len(self.indentation)
+            return max(length, self._detect_assignment_col(value, indent))
+
+        return length
+
+    def encode(self, module, stream):
+        self.assignment_col = self._detect_assignment_col(module)
+        super(PDS3LabelEncoder, self).encode(module, stream)
+
+    def encode_raw_assignment(self, key, value, level, stream):
+        indented_key = (level * self.indentation) + key
+        stream.write(indented_key.ljust(self.assignment_col))
+        stream.write(self.assignment)
+        stream.write(value)
+        stream.write(self.newline)
+ 
 
 def main():
     parser = argparse.ArgumentParser()
